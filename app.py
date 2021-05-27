ef306aea38896e60248c5432b5a0dd3d2c1737ea
5000miles <trevorczc@gmail.com>
2021-05-26 23:13:51 -0400

    update models

--- app.py
+++ app.py
@@ -1,20 +1,74 @@
 import streamlit as st
 import pickle
 import numpy as np
+import base64
+
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+      background-image: url("data:image/png;base64,%s");
+      background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+
+
+
 page = st.sidebar.selectbox(
-'Select a page:',
-('About', 'Make a prediction')
+'Navigator:',
+(
+    'About Me',
+    'NLP project',
+    'Make a prediction')
 )
 
-if page == 'About':
-    st.title('Which subreddit the post come from?')
-    '''
+if page == 'About Me':
+
+    set_png_as_page_bg('L1010977.jpg')
+
+    st.markdown("""
+    <style>
+    .big-font {
+        font-size:95px !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+    st.markdown('<p class="big-font">Hello Welcome!</p>', unsafe_allow_html=True)
+
+    st.code("class Me: \n\
+     def __init__(self, name, background, skills): \n\
+        self.name = 'Trevor' \n\
+        self.background = 'A Data Scientist/Analyst with Math and Statistics background' \n\
+        self.skills = 'Python is my first language, but I am also good at R.'\n\
+        \n\
+    class MoreMe:")
+
+
+
+if page == 'NLP project':
+    st.image('NLP/reddit.png')
+    st.title('Which subreddit is the post from?')
+    st.markdown('''
     ## Web APIs & NLP
 
     ### Description
-    This is my NLP project:  I collected text data from 2 subreddits: r/math and r/Physics, \
-    then utlizing multiple classifers to classify posts, predicting which subreddit it belongs \
-    to base on its title and self text. [You can find my full project here]('https://git.generalassemb.ly/5000miles/Submissions/tree/master/Projects/project_3-master')
+    I collected text data from 2 subreddits: `r/math` and `r/Physics`,
+    then utlizing multiple classifers to classify posts, predicting which subreddit it comes \
+    from based on its title and self text. [You can find my full project here]("https://git.gehttps://github.com/5000miles/models_practice_review/tree/main/Projects/project_3-master")
 
     #### API
 
@@ -23,7 +77,7 @@ if page == 'About':
 
     #### Data Source
 
-    Reddit:
+    Subreddits:
     > * [r/math]('https://www.reddit.com/r/math/'): 1.5m members, Created Jan 24, 2008
     > * [r/Physics]('https://www.reddit.com/r/Physics/'): 1.7m members, Created Mar 16, 2008
 
@@ -31,32 +85,41 @@ if page == 'About':
 
     ### Data info
 
-    - 5000 observations from 11/24/2020 to 3/29/2021 for each subreddit, excluded [removed] and [deleted] posts.
-        * Removed posts are results of automod action, probably because those posts triggered a filter.
-        * Deleted posts are posts that deleted by the author self.
+    - 5000 observations from 11/24/2020 to 3/29/2021 for each subreddit, excluded `[removed]` and `[deleted]` posts.
+        * `[removed]` posts are results of automod action, because those posts triggered a filter from reddit's website.
+        * `[deleted]` posts are posts that deleted by the author self.
 
 
-    - Then combination of tile & selftext is the predictor, subreddit is the response variable.
+    - The combination of `tile` & `selftext` is the predictor, `math`/`Physics` is the response variable.
 
     ---
 
-    ### Data EDA/ Preprocessing/ Visualization
+    ### EDA/ Preparing the data
 
     - Created distribuition plots for the posts in each subreddit. Clearly see that redditors like to post at around noon for both subreddit.
+    '''
+    )
+    st.image('NLP/distribution.jpg')
 
+    st.markdown(
+    '''
     - Using `RegexpTokenizer` to tokenize the predictors with Regex: `\w+`
     - Using `WordNetLemmatizer` to lemmatize the predicotrs
     - Combine 2 dateframes(math & physics)
 
     ### Model building:
 
-    Built 3 models with `GridSearchCV`:
+
+    Built 6 models with `GridSearchCV`:
 
     | Model | Vectorize |
     | --- | --- |
     | **Logistic Regression** | *Using TfidfVectorizer()* |
+    | **ordinary Decision Tree** | *Using TfidfVectorizer()* |
     | **Random Forest** | *Using TfidfVectorizer()* |
     | **Adaboost** | *Using TfidfVectorizer()* |
+    | **SVM** | *Using TfidfVectorizer()* |
+    | **Naive Bayes** | *Using TfidfVectorizer()* |
 
 
 
@@ -66,8 +129,12 @@ if page == 'About':
     | Model | Precision | Recall |
     | --- | --- | --- |
     | **Logistic Regression** | *0.866564* | *0.9040* |
+    | **SVM** | *0.858315* | *0.9208* |
+    | **ordinary Decision Tree** | *0.779221* | *0.8640* |
     | **Random Forest** | *0.832969* | *0.9136* |
     | **Adaboost** | *0.793033* | *0.9288* |
+    | **Naive Bayes** | *0.834418* | *0.9232* |
+
 
 
 
@@ -85,13 +152,19 @@ if page == 'About':
 
     ### Conclusion
 
-    The models are doing pretty good. It could be better if I could use cloud computing to speed up the fitting then I can make more adjusment on the hyperparamters.
-    '''
+    The models are doing pretty good.
+    Logistic Regression and SVM have the best performance.
+    Logistic Regression has a slightly higher precision than SVM, but SVM's recall is higher.
+    ''')
+
+    st.title('Try my model!')
 
 
-
-
-
+    st.write('\n Input a title and selftext below and my model will tell you which subreddit it comes from.')
+    model = pickle.load(open('project3.pkl','rb'))
+    # context = st.text_input("Enter the title of the Reddit post","")
+    #     if st.button('RUN!'):
+    #         if len(context) == 0
 
 if page == 'Make a prediction':
     model = pickle.load(open('project3.pkl','rb'))
