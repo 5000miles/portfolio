9c1994ebd2651213f39223bbb0d5622f835f0e00
5000miles <trevorczc@gmail.com>
2021-06-10 22:46:20 -0400

    update my intro

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ if page == 'About Me':
         self.name = 'Trevor' \n\
         self.background = 'A Data Scientist/Analyst with Math and Statistics background' \n\
         self.skills = 'Python is my first language, but I am also good at R.'\n\
-     print("Please take a look of the 2 projects I did recently and I will keep updating them"")
+     print("Please take a look of the 2 projects I did recently and I will keep updating them")\n\
+     ")
 
 
 
